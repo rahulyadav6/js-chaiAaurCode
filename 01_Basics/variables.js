18f31d6cf82ec8d1ec5136fa5051e93b4b8efc86
Rahul Kumar Yadav <116762781+rahulyadav6@users.noreply.github.com>
2023-09-15 13:36:39 +0530

    Update variables.js

--- 01_Basics/variables.js
+++ 01_Basics/variables.js
@@ -1,1 +1,17 @@
 
+"use strict "; //treat all js code ad newer version
+
+// alert("Hello");          //we are using node js not browser
+console.log("Hello");
+
+
+let name = "Rahul"
+let age = 18
+let isLoggedIn = true
+
+
+// number => 2 power 53
+// null means standalone value but undefined means not dedined yet
+
+console.log(typeof null)                     //object
+console.log(typeof undefined)                //undefined because it is a data type in javascript
